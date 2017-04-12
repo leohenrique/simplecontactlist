9205470fc8e5c359908a4280816c6e6ae11dc995
Leonardo Henrique Centenaro <leohenrique@users.noreply.github.com>
2017-04-12 11:20:01 +0100

    Implemented SQLite CRUD of Contacts

--- ContactList/app/src/main/java/henrique/leonardo/contactlist/Contact.java
+++ ContactList/app/src/main/java/henrique/leonardo/contactlist/Contact.java
@@ -1,5 +1,7 @@
 package henrique.leonardo.contactlist;
 
+import android.net.Uri;
+
 /**
  * Created by Leonardo on 06/04/2017.
  */
@@ -10,13 +12,17 @@ public class Contact {
     private String phone;
     private String email;
     private String address;
+    private Uri imageUri;
+    private int id;
 
-    public Contact(String pName, String pPhone, String pEmail, String pAddress) {
+    public Contact(int pId, String pName, String pPhone, String pEmail, String pAddress, Uri pImageURI) {
         super();
+        id = pId;
         name = pName;
         phone = pPhone;
         email = pEmail;
         address = pAddress;
+        imageUri = pImageURI;
     }
 
 
@@ -35,4 +41,9 @@ public class Contact {
     public String getAddress() {
         return address;
     }
+
+    public Uri getImageUri() { return imageUri; }
+
+    public int getId() { return id; }
+
 }
